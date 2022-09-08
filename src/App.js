d585da907b42c62a873aaaca2b8c3fa786d9bb94
rhk810 <110488142+rhk810@users.noreply.github.com>
2022-09-08 18:45:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Intro from "./components/Intro/Intro";
 import Services from "./components/Services/Services";
 import "./App.css";
 import Experience from "./components/Experience/Experience";
-import Works from "./components/Works/Works";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
@@ -24,7 +23,6 @@ function App() {
       <Intro />
       <Services />
       <Experience />
-      {/* <Works /> */}
       <Portfolio />
       <Contact />
       <Footer />
