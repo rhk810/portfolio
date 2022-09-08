731ccdd6d22d18c44ad6ee6121cef728c4abfe06
rhk810 <110488142+rhk810@users.noreply.github.com>
2022-09-08 18:53:25 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useContext, useRef} from "react";
 import "./Contact.css";
 import { themeContext } from "../../Context";
 import down from "../../img/down1.png";
